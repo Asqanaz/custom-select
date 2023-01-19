63673a7999ae39517a05f48dd339466e009f02a2
Asqanaz <asmoyan.04@gmail.com>
2023-01-20 00:19:08 +0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import { useState } from 'react'
+import { useState } from "react"
+import { MultiSelect } from "./MultiSelect"
+import { SingleSelect } from "./SingleSelect"
+
+const options = [
+	{
+		id: 1,
+		label: "A",
+		value: "a"
+	},
+	{
+		id: 2,
+		label: "B",
+		value: "b"
+	},
+	{
+		id: 3,
+		label: "C",
+		value: "c"
+	},
+	{
+		id: 4,
+		label: "D",
+		value: "d"
+	}
+]
 
 function App() {
-
-  return (
-    <div className="App">
-      
-    </div>
-  )
+	return (
+		<div className="App bg-black min-h-screen w-full flex flex-col gap-y-20 justify-center items-center">
+			<MultiSelect selectOptions = {options}/>
+			<SingleSelect selectOptions = {options}/>
+		</div>
+	)
 }
 
 export default App
---
+++ src/MultiSelect.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useRef, useEffect } from "react"
+
+export const MultiSelect = ({ selectOptions }) => {
+	const [isOpen, setIsOpen] = useState(false)
+	const [options, setOptions] = useState(selectOptions)
+	const [selectedOption, setSelectedOption] = useState([])
+
+	const handleOptionDelete = option => {
+		setSelectedOption(selectedOption.filter(op => op.value !== option.value))
+		setOptions([...options, option].sort((prev, curr) => prev.id - curr.id))
+	}
+
+	const handleAddToOptions = option => {
+		setSelectedOption([...selectedOption, option])
+		setOptions(options.filter(item => item.value !== option.value))
+	}
+
+	const list = useRef()
+	useEffect(() => {
+		document.addEventListener("click", handleDocumentClicked)
+		return () => document.removeEventListener("click", handleDocumentClicked)
+	}, [])
+
+	const handleDocumentClicked = e => {
+		if (list.current && list.current.contains(e.target)) {
+			setIsOpen(true)
+			
+		} else {
+			setIsOpen(false)
+		}
+		console.log(e.composedPath().includes(list))
+		console.log(e.composedPath())
+	}
+
+	return (
+		<div className="w-1/2 min-h-[50px] relative bg-gray-200 border-2 border-green-200 mx-auto rounded-full px-5 p-2 flex items-center">
+			<div
+				className="w-full h-full flex items-center gap-x-8"
+				role="presentation"
+				ref = {list}
+			>
+				{selectedOption.length
+					? selectedOption.map(item => (
+							<div
+								className="h-full bg-gray-500 flex gap-x-5 w-fit items-center justify-between"
+								key={item.id}
+							>
+								<span>{item.label}</span>
+								<button onClick={() => handleOptionDelete(item)} 
+								tabIndex = {0}
+								aria-hidden = {false}
+								>X</button>
+							</div>
+					  ))
+					: "MultiSelect"}
+			</div>
+			<div className="absolute right-[20px] h-full flex items-center gap-5">
+				<div className="h-[80%] rounded-full my-auto w-0 border border-black" />
+				<button className="text-black text-[20px]" onClick={() => setIsOpen(false)}>
+					X
+				</button>
+			</div>
+
+			{isOpen && (
+				<ul role = "presentation" className="w-full absolute top-full left-0 h-fit  bg-gray-300 rounded-2xl z-20" onClick = {(e) => e.stopPropagation()}>
+					{options.length ? (
+						options.map(item => (
+							<li
+								className='p-5'
+								role="presentation"
+								key={item.id}
+								onClick={() => handleAddToOptions(item)}
+							>
+								{item.label}
+							</li>
+						))
+					) : (
+						<li className="p-5">No select element</li>
+					)}
+				</ul>
+			)}
+		</div>
+	)
+}
